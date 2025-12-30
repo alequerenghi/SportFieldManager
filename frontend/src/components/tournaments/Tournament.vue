c761d28f5879cfd4ed826999247319c0fb9039bf
alequerenghi <querenghialessandro@icloud.com>
2025-12-31 00:35:27 +0100

    routing Tournament

--- frontend/src/components/tournaments/Tournament.vue
+++ frontend/src/components/tournaments/Tournament.vue
@@ -1,26 +1,23 @@
 <script setup>
 import { onMounted, ref, watch } from "vue";
 import Schedule from "./Schedule.vue";
+import { useRoute, useRouter } from "vue-router";
 
-const props = defineProps({
-  tournament: { type: Object, required: true },
-});
-
-const emit = defineEmits(["update-state"]);
-
+const router = useRouter();
+const route = useRoute();
 const field = ref({});
 const creator = ref({});
 const userLogged = ref(false);
 const pMessage = ref("");
-const addingTeams = ref("no");
+const modifying = ref("no");
 const newTeam = ref("");
-const teams = ref([]);
-const newMaxTeams = ref(props.tournament.maxTeams);
+const newMaxTeams = ref(0);
 const newTeamName = ref("");
 const showSchedule = ref(false);
+const tournament = ref(null);
 
 const deleteTournament = async () => {
-  const response = await fetch(`/api/tournaments/${props.tournament._id}`, {
+  const response = await fetch(`/api/tournaments/${route.params.id}`, {
     method: "DELETE",
     credentials: "include",
   });
@@ -28,35 +25,39 @@ const deleteTournament = async () => {
     const data = await response.json();
     console.error(data.error);
   }
-  emit("update-state", "searching");
+  router.push("/");
 };
 
 const updateTournamentInfo = async () => {
-  const payload = {
-    teams: teams.value,
-    maxTeams: newMaxTeams.value,
-  };
-  if (newTeamName.value.length < 3) {
-    payload.name = props.tournament.name;
-  } else {
-    payload.name = newTeamName.value;
+  try {
+    const payload = {
+      teams: tournament.value.teams,
+      maxTeams: newMaxTeams.value,
+    };
+    if (newTeamName.value.length < 3) {
+      payload.name = tournament.value.name;
+    } else {
+      payload.name = newTeamName.value;
+    }
+    const response = await fetch(`/api/tournaments/${route.params.id}`, {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(payload),
+      credentials: "include",
+    });
+    if (!response.ok) {
+      const { error } = await response.json();
+      pMessage.value = error;
+    }
+  } catch (error) {
+    console.error(error);
   }
-  const response = await fetch(`/api/tournaments/${props.tournament._id}`, {
-    method: "PUT",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(payload),
-    credentials: "include",
-  });
-  if (!response.ok) {
-    const { error } = await response.json();
-    pMessage.value = error;
-  }
-  emit("update-state", "searching");
+  await loadTournament();
 };
 
 const generate = async () => {
   const response = await fetch(
-    `/api/tournaments/${props.tournament._id}/matches/generate`,
+    `/api/tournaments/${route.params.id}/matches/generate`,
     {
       method: "POST",
       credentials: "include",
@@ -67,78 +68,93 @@ const generate = async () => {
     pMessage.value = await response.text();
   }
 };
+const loadTournament = async () => {
+  const response = await fetch(`/api/tournaments/${route.params.id}`, {
+    credentials: "include",
+  });
+  tournament.value = await response.json();
+};
 
+const addTeam = () => {
+  tournament.value.teams.push(newTeam.value);
+  newTeam.value = "";
+};
 onMounted(async () => {
-  const response = await fetch(`/api/fields/${props.tournament.fieldId}`);
-  field.value = await response.json();
-  const creatorResponse = await fetch(`/api/users/${props.tournament.userId}`);
+  await loadTournament();
+  const { fieldId, maxTeams, userId } = tournament.value;
+  newMaxTeams.value = maxTeams;
+
+  const fieldInfo = await fetch(`/api/fields/${fieldId}`);
+  field.value = await fieldInfo.json();
+
+  const creatorResponse = await fetch(`/api/users/${userId}`);
   creator.value = await creatorResponse.json();
   const whoami = await fetch("/api/whoami", { credentials: "include" });
   if (whoami.ok) {
     const user = await whoami.json();
     const { _id } = user;
-    if (_id === props.tournament.userId) {
+    if (_id === userId) {
       userLogged.value = true;
     }
   }
 });
-
-watch(
-  () => props.tournament.teams,
-  (newTeam) => (teams.value = [...newTeam]),
-  { immediate: true }
-);
 </script>
 
 <template>
-  <h1>{{ tournament.name }}</h1>
-  <div v-if="showSchedule">
-    <Schedule :tournamentId="tournament._id" />
-  </div>
-  <div v-else>
-    <ul>
-      <li>Field: {{ tournament.sport }}</li>
-      <li>Max teams: {{ tournament.maxTeams }}</li>
-      <li>Field: {{ field.name }}</li>
-      <li>Location: {{ field.location }}</li>
-      <li>Created by: {{ creator.username }}</li>
-      <li>Start date: {{ new Date(tournament.startDate).toDateString() }}</li>
-      <li>Teams:</li>
-      <ul>
-        <li v-for="team in teams">
-          <a>{{ team }}</a>
-        </li>
-      </ul>
-      <li v-if="tournament.schedule">
-        <a @click="showSchedule = true">Schedule</a>
-      </li>
-      <form
-        v-if="addingTeams === 'teams'"
-        @submit.prevent="updateTournamentInfo"
-      >
-        <input type="text" placeholder="Team Name" v-model="newTeam" />
-        <button @click.prevent="teams.push(newTeam)">Add one</button>
-        <input type="submit" value="Send data" />
-      </form>
-      <form
-        v-else-if="addingTeams === 'info'"
-        @submit.prevent="updateTournamentInfo"
-      >
-        <input type="text" placeholder="New Team Name" v-model="newTeamName" />
-        <input
-          type="number"
-          placeholder="{{ tournament.maxTeams }}"
-          v-model="newMaxTeams"
-        />
-        <input type="submit" value="Send data" />
-      </form>
-    </ul>
-    <div v-if="userLogged">
-      <button @click="addingTeams = 'info'">Edit info</button>
-      <button @click="addingTeams = 'teams'">Add team</button>
-      <button @click.prevent="deleteTournament">Delete</button>
-      <button @click="generate" v-if="!tournament.schedule">Generate</button>
+  <div v-if="tournament">
+    <h1>{{ tournament.name }}</h1>
+    <div v-if="showSchedule">
+      <Schedule :tournamentId="route.params.id" />
     </div>
-    <p>{{ pMessage }}</p>
+    <div v-else>
+      <ul>
+        <li>Field: {{ tournament.sport }}</li>
+        <li>Max teams: {{ tournament.maxTeams }}</li>
+        <li>Field: {{ field.name }}</li>
+        <li>Location: {{ field.location }}</li>
+        <li>Created by: {{ creator.username }}</li>
+        <li>Start date: {{ new Date(tournament.startDate).toDateString() }}</li>
+        <li>Teams:</li>
+        <ul>
+          <li v-for="team in tournament.teams">
+            <a>{{ team }}</a>
+          </li>
+        </ul>
+        <li v-if="tournament.schedule">
+          <a @click="showSchedule = true">Schedule</a>
+        </li>
+        <form
+          v-if="modifying === 'teams'"
+          @submit.prevent="updateTournamentInfo"
+        >
+          <input type="text" placeholder="Team Name" v-model="newTeam" />
+          <button @click.prevent="addTeam">Add one</button>
+          <input type="submit" value="Send data" />
+        </form>
+        <form
+          v-else-if="modifying === 'info'"
+          @submit.prevent="updateTournamentInfo"
+        >
+          <input
+            type="text"
+            placeholder="New Team Name"
+            v-model="newTeamName"
+          />
+          <input
+            type="number"
+            placeholder="{{ tournament.maxTeams }}"
+            v-model="newMaxTeams"
+          />
+          <input type="submit" value="Send data" />
+        </form>
+      </ul>
+      <div v-if="userLogged">
+        <button @click="modifying = 'info'">Edit info</button>
+        <button @click="modifying = 'teams'">Add team</button>
+        <button @click.prevent="deleteTournament">Delete</button>
+        <button @click="generate" v-if="!tournament.schedule">Generate</button>
+      </div>
+      <p>{{ pMessage }}</p>
+    </div>
   </div>
 </template>
