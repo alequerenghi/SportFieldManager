c09f4e8b170cb439d32467d4f0aff259f90cb75a
alequerenghi <querenghialessandro@icloud.com>
2025-12-31 00:35:08 +0100

    routing Field

--- frontend/src/components/Field.vue
+++ frontend/src/components/Field.vue
@@ -1,21 +1,17 @@
 <script setup>
 import { onMounted, ref, watch } from "vue";
-
-const props = defineProps({
-  field: {
-    type: Object,
-    required: true,
-  },
-});
+import { useRoute } from "vue-router";
 
 const message = ref("");
 const calendar = ref("");
 const slots = ref([]);
+const route = useRoute();
+const field = ref(null);
 
 watch(calendar, async (date) => {
   console.log(date);
   const response = await fetch(
-    `/api/fields/${props.field._id}/slots?date=${date}`
+    `/api/fields/${route.params.id}/slots?date=${date}`
   );
   if (!response.ok) {
     message.value = await response.json();
@@ -37,7 +33,7 @@ watch(calendar, async (date) => {
 });
 
 const makeBooking = async (slot) => {
-  const response = await fetch(`/api/fields/${props.field._id}/bookings`, {
+  const response = await fetch(`/api/fields/${route.params.id}/bookings`, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ date: calendar.value, slot: slot.slot }),
@@ -57,7 +53,7 @@ const makeBooking = async (slot) => {
 
 const deleteBooking = async (slot) => {
   const response = await fetch(
-    `/api/fields/${props.field._id}/bookings/${slot._id}`,
+    `/api/fields/${route.params._id}/bookings/${slot._id}`,
     {
       method: "DELETE",
       credentials: "include",
@@ -76,28 +72,34 @@ const deleteBooking = async (slot) => {
 };
 
 onMounted(() => (calendar.value = new Date().toISOString().substring(0, 10)));
+onMounted(async () => {
+  const response = await fetch(`/api/fields/${route.params.id}`);
+  field.value = await response.json();
+});
 </script>
 
 <template>
-  <h1>{{ field.name }}</h1>
-  <ul>
-    <li>Field: {{ field.sport }}</li>
-    <li>Location: {{ field.location }}</li>
-    <li>
-      Opening Hours:
-      {{ `${field.slots[0]}-${field.slots[field.slots.length - 1]}` }}
-    </li>
-    <form>
-      <input type="date" v-model="calendar" />
-    </form>
+  <div v-if="field">
+    <h1>{{ field.name }}</h1>
     <ul>
-      <li v-for="slot in slots">
-        <button @click="makeBooking(slot)" :disabled="!slot.available">
-          {{ slot.slot }}
-        </button>
-        <button v-if="slot.me" @click="deleteBooking(slot)">DELETE</button>
+      <li>Field: {{ field.sport }}</li>
+      <li>Location: {{ field.location }}</li>
+      <li>
+        Opening Hours:
+        {{ `${field.slots[0]}-${field.slots[field.slots.length - 1]}` }}
       </li>
-      <p>{{ message }}</p>
+      <form>
+        <input type="date" v-model="calendar" />
+      </form>
+      <ul>
+        <li v-for="slot in slots">
+          <button @click="makeBooking(slot)" :disabled="!slot.available">
+            {{ slot.slot }}
+          </button>
+          <button v-if="slot.me" @click="deleteBooking(slot)">DELETE</button>
+        </li>
+        <p>{{ message }}</p>
+      </ul>
     </ul>
-  </ul>
+  </div>
 </template>
