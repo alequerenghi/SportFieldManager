26fc9923f4797622a49b52ed8e530e7d77b93305
alequerenghi <querenghialessandro@icloud.com>
2026-01-02 20:35:40 +0100

    fix: field

--- frontend/src/components/Field.vue
+++ frontend/src/components/Field.vue
@@ -2,37 +2,15 @@
 import { onMounted, ref, watch } from "vue";
 import { useRoute } from "vue-router";
 
-const message = ref("");
+const errorMessage = ref("");
 const calendar = ref("");
 const slots = ref([]);
 const route = useRoute();
 const field = ref(null);
-
-watch(calendar, async (date) => {
-  console.log(date);
-  const response = await fetch(
-    `/api/fields/${route.params.id}/slots?date=${date}`
-  );
-  if (!response.ok) {
-    message.value = await response.json();
-  } else {
-    message.value = "";
-    const data = await response.json();
-    const whoami = await fetch("/api/whoami", {
-      credentials: "include",
-    });
-    if (!whoami.ok) {
-      data.forEach((element) => {
-        element.me = false;
-        element.available = false;
-      });
-      message.value = "Please sign in first";
-    }
-    slots.value = data;
-  }
-});
+const authenticated = ref(false);
 
 const makeBooking = async (slot) => {
+  errorMessage.value = "";
   const response = await fetch(`/api/fields/${route.params.id}/bookings`, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
@@ -41,10 +19,10 @@ const makeBooking = async (slot) => {
   });
   if (!response.ok) {
     const { err } = await response.json();
-    message.value = err;
+    errorMessage.value = err;
   } else {
     const { bookingId } = await response.json();
-    message.value = "";
+    errorMessage.value = "";
     slot.me = true;
     slot.available = false;
     slot._id = bookingId;
@@ -52,29 +30,45 @@ const makeBooking = async (slot) => {
 };
 
 const deleteBooking = async (slot) => {
+  errorMessage.value = "";
   const response = await fetch(
-    `/api/fields/${route.params._id}/bookings/${slot._id}`,
+    `/api/fields/${route.params.id}/bookings/${slot._id}`,
     {
       method: "DELETE",
       credentials: "include",
     }
   );
   if (!response.ok) {
-    const { error } = await response.json();
-    message.value = error;
+    errorMessage.value = await response.text();
   } else {
     slot.available = true;
     slot.me = false;
     slot._id = null;
-    const { message: data } = await response.json();
-    message.value = data;
   }
 };
 
-onMounted(() => (calendar.value = new Date().toISOString().substring(0, 10)));
+watch(calendar, async (date) => {
+  errorMessage.value = "";
+  const response = await fetch(
+    `/api/fields/${route.params.id}/slots?date=${date}`
+  );
+  if (!response.ok) {
+    errorMessage.value = await response.text();
+  } else {
+    slots.value = await response.json();
+  }
+});
+
 onMounted(async () => {
+  calendar.value = new Date().toISOString().substring(0, 10);
   const response = await fetch(`/api/fields/${route.params.id}`);
   field.value = await response.json();
+  const whoami = await fetch("/api/whoami", {
+    credentials: "include",
+  });
+  if (whoami.ok) {
+    authenticated.value = true;
+  }
 });
 </script>
 
@@ -82,7 +76,7 @@ onMounted(async () => {
   <div v-if="field">
     <h1>{{ field.name }}</h1>
     <ul>
-      <li>Field: {{ field.sport }}</li>
+      <li>Sport: {{ field.sport }}</li>
       <li>Location: {{ field.location }}</li>
       <li>
         Opening Hours:
@@ -91,15 +85,15 @@ onMounted(async () => {
       <form>
         <input type="date" v-model="calendar" />
       </form>
-      <ul>
-        <li v-for="slot in slots">
+      <ul v-if="authenticated">
+        <li v-for="slot in slots" :key="slot.slot">
           <button @click="makeBooking(slot)" :disabled="!slot.available">
             {{ slot.slot }}
           </button>
           <button v-if="slot.me" @click="deleteBooking(slot)">DELETE</button>
         </li>
-        <p>{{ message }}</p>
       </ul>
     </ul>
+    <p>{{ errorMessage }}</p>
   </div>
 </template>
