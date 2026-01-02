626033f9ba74e482054b4940dcc0b279d41e7e75
alequerenghi <querenghialessandro@icloud.com>
2026-01-02 21:42:22 +0100

    update: added users and teams search

--- frontend/src/components/Search.vue
+++ frontend/src/components/Search.vue
@@ -1,57 +1,63 @@
 <script setup>
-import { ref, watch } from "vue";
-import Field from "./Field.vue";
+import { onMounted, ref, watch } from "vue";
 import { searchSingle } from "./utils.js";
-import Tournament from "./tournaments/Tournament.vue";
-import { useRouter } from "vue-router";
 
-const mainPage = ref("searching");
 const fields = ref([]);
 const tournaments = ref([]);
-const searchBar = ref(" ");
+const teams = ref([]);
+const players = ref([]);
+const searchBar = ref("");
 let debounce = null;
-const router = useRouter();
 
 const search = async (val) => {
   try {
     fields.value = await searchSingle("fields", val);
     tournaments.value = await searchSingle("tournaments", val);
+    teams.value = await searchSingle("teams", val);
+    players.value = await searchSingle("players", val);
   } catch (err) {
     console.error(err);
   }
 };
 
-watch(searchBar, async (val) => {
+watch(searchBar, (val) => {
   clearTimeout(debounce);
-  debounce = setTimeout(await search(val), 400);
+  debounce = setTimeout(async () => await search(val), 400);
 });
 
-const fieldDetails = async (fieldId) => {
-  const response = await fetch(`/api/fields/${fieldId}`);
-  if (!response.ok) {
-    // TODO
-  } else {
-    const data = await response.json();
-    router.push(`/fields/${data._id}`);
-  }
-};
+onMounted(async () => {
+  await search("");
+});
 </script>
 
 <template>
-  <input type="text" placeholder="Search" v-model="searchBar" />
+  <input placeholder="Search" v-model="searchBar" />
   <div>
     <h2 v-if="fields.length">Fields</h2>
     <ul>
-      <li v-for="field in fields">
-        <a @click.prevent="fieldDetails(field._id)">{{ field.name }}</a>
+      <li v-for="field in fields" :key="field.name">
+        <RouterLink :to="`/fields/${field._id}`">{{ field.name }}</RouterLink>
       </li>
     </ul>
     <h2 v-if="tournaments.length">Tournaments</h2>
     <ul>
-      <li v-for="tournament in tournaments">
+      <li v-for="tournament in tournaments" :key="tournament.name">
         <RouterLink :to="`/tournaments/${tournament._id}`">{{
           tournament.name
         }}</RouterLink>
+      </li>
+    </ul>
+    <h2 v-if="teams.length">Teams</h2>
+    <ul>
+      <li v-for="team in teams" :key="team.name">
+        <RouterLink :to="`/teams/${team._id}`">{{ team.name }}</RouterLink>
+      </li>
+    </ul>
+    <h2 v-if="players.length">Players</h2>
+    <ul>
+      <li v-for="player in players" :key="player.surname">
+        {{ player.name }} {{ player.surname }}, {{ player.team
+        }}{{ player.jerseyNumber ? `with number: ${player.jerseyNumber}` : "" }}
       </li>
     </ul>
   </div>
